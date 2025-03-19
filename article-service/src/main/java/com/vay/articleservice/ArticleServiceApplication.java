dca470345ff958f54be04cb8ae236967c86bd69b
vay <prop615111@gmail.com>
2025-03-19 21:39:16 +1000

    init

--- article-service/src/main/java/com/vay/articleservice/ArticleServiceApplication.java
+++ article-service/src/main/java/com/vay/articleservice/ArticleServiceApplication.java
@@ -1,13 +1,13 @@
-package com.vay.messanger;
+package com.vay.articleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessengerApplication {
+public class ArticleServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MessengerApplication.class, args);
+        SpringApplication.run(ArticleServiceApplication.class, args);
     }
 
 }
--- article-service/src/test/java/com/vay/articleservice/ArticleServiceApplicationTests.java
+++ article-service/src/test/java/com/vay/articleservice/ArticleServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.vay.messanger;
+package com.vay.articleservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MessengerApplicationTests {
+class ArticleServiceApplicationTests {
 
     @Test
     void contextLoads() {
