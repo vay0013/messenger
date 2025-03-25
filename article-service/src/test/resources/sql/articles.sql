30e243b576cc325a87611cdbbdd5aed6c29afe01
vay <prop615111@gmail.com>
2025-03-25 21:53:13 +1000

    test

---
+++ article-service/src/test/resources/sql/articles.sql
@@ -0,0 +1,5 @@
+insert into article.t_articles (id, title, content, created_at)
+values (1, 'c1', 't1', now()),
+       (2, 'c2', 't2', now()),
+       (3, 'c3', 't3', now()),
+       (4, 'c4', 't4', now());
